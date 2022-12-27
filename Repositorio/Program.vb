e63aca7a949e128270d5f84328a3568035fabb48
Hazael Roberto Martinez Sanchez <hazapunkk@gmail.com>
2022-12-27 12:49:33 -0600

    .

--- Repositorio/Program.vb
+++ Repositorio/Program.vb
@@ -2,7 +2,7 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Console.WriteLine("soy alondra y no me gusta seguir indicaciones para hacer bien las cosas (soy zoquete)!")
     End Sub
 
 End Module
