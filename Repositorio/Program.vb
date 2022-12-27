b4cea8d2c93411510dd7e82077cd82ecfb2fb903
Alondra <Alondra@VS-DESARROLLO5>
2022-12-27 12:36:11 -0600

    cambio alondra

--- Repositorio/Program.vb
+++ Repositorio/Program.vb
@@ -2,6 +2,6 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello Alondra probando GIT!")
+        Console.WriteLine("Hello Alondra probando git hub!")
     End Sub
 End Module
