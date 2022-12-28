ea4e21f0052e1f9b91d3697dee3c7a5f8e3fb115
Alondra <Alondra@VS-DESARROLLO5>
2022-12-28 17:34:30 -0600

    cambio admin alondra

--- Repositorio/Program.vb
+++ Repositorio/Program.vb
@@ -2,7 +2,7 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("soy alondra y no me gusta seguir indicaciones para hacer bien las cosas (soy zoquete)!")
+        Console.WriteLine("Soy alondra y soy la Admin")
     End Sub
 
 End Module
