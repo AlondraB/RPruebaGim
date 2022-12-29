a6b0b556d4466a01c5064f65deaf2b4bef81513a
Alondra <Alondra@VS-DESARROLLO5>
2022-12-28 18:18:25 -0600

    bere2

--- Repositorio/Program.vb
+++ Repositorio/Program.vb
@@ -3,6 +3,8 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Esta es una prueba de Berenice")
+        Console.WriteLine("Berenice")
+
     End Sub
 
 End Module
